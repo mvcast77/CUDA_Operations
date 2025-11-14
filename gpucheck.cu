0930fb16fc8ea7eeb33bbae7064639ef5efef3c6
Michael Castillo <castillo2020var@gmail.com>
2025-11-14 12:12:18 -0800

    Handle no device found

--- gpucheck.cu
+++ gpucheck.cu
@@ -4,21 +4,22 @@ void checkGPU(){
 	int dev_check = 0;
 	cudaGetDeviceCount(&dev_check);
 
-	if (dev_check) { std::cout << "No GPU available\n"; return; }
+	if (!dev_check) { std::cout << "No GPU available\n"; return; }
+	else if (dev_check > 1) std::cout << "More than one GPU is available\n\n";
 
 	cudaDeviceProp info;
 	int deviceID = 0;
 	cudaGetDevice(&deviceID);
 	cudaGetDeviceProperties(&info, deviceID);
 	
-	std::cout << "Name: " << info.name << std::endl;
-	std::cout << "Major/minor: " << info.major << "." << info.minor << std::endl;
-	std::cout << "Warp Size: " << info.warpSize << std::endl;
-	std::cout << "SM number: " << info.multiProcessorCount << std::endl;
-	std::cout << "Max threads per SM: " << info.maxThreadsPerMultiProcessor << std::endl;
-	std::cout << "Max threads per block: " << info.maxThreadsPerBlock << std::endl;
-	std::cout << "Memory Specifications" << std::endl;
-	std::cout << "Total shared memory for multiprocesser: " << info.sharedMemPerBlock << " bytes" << std::endl;
+	std::cout << "Name: " << info.name << std::endl \
+	<< "Major/minor: " << info.major << "." << info.minor << std::endl \
+	<< "Warp Size: " << info.warpSize << std::endl \
+	<< "SM number: " << info.multiProcessorCount << std::endl \
+	<< "Max threads per SM: " << info.maxThreadsPerMultiProcessor << std::endl \
+	<< "Max threads per block: " << info.maxThreadsPerBlock << std::endl \
+	<< "Memory Specifications" << std::endl \
+	<< "Total shared memory for multiprocesser: " << info.sharedMemPerBlock << " bytes" << std::endl;
 }
 
 int main(int argc, char ** argv) {
